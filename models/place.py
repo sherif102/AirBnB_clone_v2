91a5075600c396fcb94f208c964cbfa59bc249b1
hyperbayse <hyperbayse@gmail.com>
2025-02-02 10:44:34 +0100

    Added the Base inheritance to Place

--- models/place.py
+++ models/place.py
@@ -4,7 +4,7 @@ from models.base_model import BaseModel, Base, Column, String, Integer, Float
 from models.base_model import ForeignKey, relationship
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
