1dd36083b59a609593ff277eeca99290b53b16bf
hyperbayse <hyperbayse@gmail.com>
2025-02-02 11:17:05 +0100

    corrected few mistakes

--- test.py
+++ test.py
@@ -14,3 +14,28 @@ print(st)
 #                                                      '%Y-%m-%dT%H:%M:%S.%f')
 # kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
 #                                                      '%Y-%m-%dT%H:%M:%S.%f')
+
+# @property
+    # def reviews(self):
+    #     """return the list of review instance linked"""
+    #     from models import storage
+    #     from models.review import Review
+    #     reviews = []
+    #     all = storage.all(Review)
+    #     for key, value in all.items():
+    #         if self.id == value.place_id:
+    #             reviews.append(value)
+    #     return reviews
+
+
+      # @property
+    # def cities(self):
+    #     """return the list of City instance linked"""
+    #     from models import storage
+    #     from models.city import City
+    #     cities = []
+    #     all = storage.all(City)
+    #     for key, value in all.items():
+    #         if self.id == value.state_id:
+    #             cities.append(value)
+    #     return cities
