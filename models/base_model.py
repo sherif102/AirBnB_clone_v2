04cbb5ebb1b00d21acc21242cfc3f5d8f015d6a7
hyperbayse <hyperbayse@gmail.com>
2025-02-02 10:40:18 +0100

    mapped the Place, User and City tables and added relationship

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
+from sqlalchemy import Column, String, Integer, ForeignKey, DateTime, Float
 from sqlalchemy.orm import relationship
 
 
