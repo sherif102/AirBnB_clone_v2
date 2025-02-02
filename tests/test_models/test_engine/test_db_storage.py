68bf05229a21ff3fa4b1968f7c834d6d30a554eb
hyperbayse <hyperbayse@gmail.com>
2025-02-02 16:59:59 +0100

    updated to testfile test_db_storage.py

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -3,6 +3,7 @@
 import unittest
 from models.base_model import BaseModel
 from models.__init__ import storage
+from models import *
 
 
 class test_dbStorage(unittest.TestCase):
@@ -13,15 +14,21 @@ class test_dbStorage(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_db_connection(self):
+    def test_save(self):
         """test the db connection is an object"""
-        connection = storage.con
-        self.assertIsInstance(connection, object)
+        storage.new(State(name="Ekiti"))
+        storage.save()
+        db_len = len(storage.all(State))
+
+        storage.new(State(name="Osun"))
+        storage.save()
+
+        self.assertEqual(db_len, len(storage.all(State)) - 1)
 
     def test_all_return(self):
         """test attributes in the DBStorage class"""
-        columns = storage.all('states')
-        self.assertIsInstance(columns, tuple)
+        columns = storage.all(State)
+        self.assertIsInstance(columns, dict)
 
 
 if __name__ == "__main__":
