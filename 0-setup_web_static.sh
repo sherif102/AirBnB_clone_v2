5123c20069e628de517edad391195c0047607ae3
hyperbayse <hyperbayse@gmail.com>
2025-02-19 02:28:06 +0100

    script that setup the virtual service with nginx running

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# setup nginx environment for deployment
+if ! command nginx &> /dev/null; then
+    sudo apt update
+    sudo apt install -y nginx
+fi
+sudo mkdir -p /data/web_static/{releases/test,shared}
+echo "It works!" | sudo tee -a /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.bak
+sudo sed -i '0,/server_name _;/s//&\n   location \/hbnb_static\/ {\n        alias \/data\/web_static\/current\/; \n     }/' /etc/nginx/sites-available/default
+sudo service nginx start
