bd34a009e8183c5cc0d3e1159229a5ef02152d95
hyperbayse <hyperbayse@gmail.com>
2025-03-05 19:15:08 +0100

    correction

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -48,7 +48,7 @@ def number(n):
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
     """return response"""
-    return render_template("5-number.html", number=n)
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
