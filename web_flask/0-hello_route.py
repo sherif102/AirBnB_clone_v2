b2174f557f4994040105bb35e056712a5a9f1cce
hyperbayse <hyperbayse@gmail.com>
2025-03-05 16:08:07 +0100

    flask run script

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+"""Flask app"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """return the home"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
