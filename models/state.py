29c97df26404282669e075ec28e17be011b7f8bf
hyperbayse <hyperbayse@gmail.com>
2025-03-06 14:33:06 +0100

    correction

--- models/state.py
+++ models/state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base, Column, String, relationship
-from os import getenv
+# from os import getenv
 
 
 class State(BaseModel, Base):
@@ -10,12 +10,12 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", back_populates="state")
 
-    if getenv('HBNB_TYPE_STORAGE') != 'db':
-        def cities(self):
-            """return the list of City"""
-            cities = []
-            objects = storage.all()
-            for obj in objects.values():
-                if getattr(obj, 'state_id') == self.id:
-                    cities.append(obj)
-            return cities
+    # if getenv('HBNB_TYPE_STORAGE') != 'db':
+    #     def cities(self):
+    #         """return the list of City"""
+    #         cities = []
+    #         objects = storage.all()
+    #         for obj in objects.values():
+    #             if getattr(obj, 'state_id') == self.id:
+    #                 cities.append(obj)
+    #         return cities
